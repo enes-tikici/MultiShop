9a3362904c1e8c4484804c086729159b5d1b8fb9
enes-tikici <enes.tikici123@hotmail.com>
2026-02-23 01:24:05 +0300

    added frontend in product,product list controller ant components

---
+++ Frontends/MultiShop.WebUI/ViewComponents/UILayoutViewComponenets/_TopbarUILayoutComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.UILayoutViewComponenets
+{
+    public class _TopbarUILayoutComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
